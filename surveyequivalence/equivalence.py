cb987b2a43462f05a4e3f2b041fe3586504e1b02
tweninger <tweninge@nd.edu>
2020-04-28 12:10:54 -0400

    Merge presnick branch.
    add print statement for long running anon bayes combiner
    
    plot output goes into proper folder with windows-available name

--- surveyequivalence/equivalence.py
+++ surveyequivalence/equivalence.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Sequence, Dict, Tuple, Callable
 import numpy as np
 import pandas as pd
+import os
 import random
 
 from .combiners import Prediction
@@ -64,7 +65,7 @@ class AnalysisPipeline:
 
     def __init__(self,
                  W: pd.DataFrame,
-                 combiner: Callable[[Sequence[str], np.array, str, str], Prediction],
+                 combiner: Callable,
                  scoring_function: Callable[[Sequence[Prediction], Sequence[str]], float],
                  allowable_labels: Sequence[str],
                  null_prediction: Prediction,
@@ -101,6 +102,7 @@ class AnalysisPipeline:
         N = len(self.W)
 
         for k in range(1,K+1):
+            print(k)
 
             # Sample N rows from the rating matrix W with replacement
             I = self.W[np.random.choice(self.W.shape[0], N, replace=True)]
@@ -130,7 +132,7 @@ class AnalysisPipeline:
                 if k==0:
                     pred = self.null_prediction
                 else:
-                    pred = self.combiner(self.allowable_labels, rating_tups[0:-1])
+                    pred = self.combiner(self.allowable_labels, rating_tups[0:-1], self.W)
                 predictions.append(pred)
 
             result[k] = self.scoring_function(predictions, reference_ratings)
@@ -147,8 +149,9 @@ class AnalysisPipeline:
         ax.set_xlabel(xlabel, fontsize=16)
         ax.set_ylabel(ylabel, fontsize=16)
         plt.legend(loc='upper right')
-
-        plt.savefig(f'plots/power_curve{str(datetime.now())[:-5]}.png')
+        if not os.path.isdir('plots'):
+            os.mkdir('plots')
+        plt.savefig(f'plots/power_curve{datetime.now().strftime("%d-%m-%Y_%I-%M-%S_%p")}.png')
 
         pass
 
