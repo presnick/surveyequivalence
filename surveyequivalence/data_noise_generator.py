e5897ff0ffca8e538aad093c7af8ee7ed2c0b7a8
Paul Resnick <resnick.paul@gmail.com>
2021-10-22 14:22:22 -0400

    Update data_noise_generator.py

--- surveyequivalence/data_noise_generator.py
+++ surveyequivalence/data_noise_generator.py
@@ -15,31 +15,44 @@ class BernoulliNoise:
         self.p=p
     def draw(self):
         r=random.random()
-        if(r<self.p):
-            return 1
-        else:
-            return 0
+        return r<self.p
 
-def noisy_wiki_attack(dirname='',inputfile='wiki_attack_labels_and_predictor.csv',outputfile='wiki_attack_labels_and_predictor.csv',new_file=1,noise=BernoulliNoise(0.1)):
+def noisy_wiki_attack(dirname='',inputfile='wiki_attack_labels_and_predictor.csv',outputfile='wiki_attack_labels_and_predictor.csv',new_file=True,noise=BernoulliNoise(0.1)):
     """
-        Generate noise based on wiki_attack_labels_and_predictor dataset
-        default noise type is bernoulli(0.1)
+        Load the wiki_attack_labels_and_predictor dataset and add some noise to the labels
+        
+        Parameters
+        ----------
+        dirname: directory string
+        inputfile: filename, a string
+        outputfile: filename, a string
+        new_file: if True, append timestamp to output filename
+        noise: an instance of BernoulliNoise, which returns True with some probability, indicating that we should add noise to that instance
+
+        Returns
+        -------
+        None
+        
     """
+
     path = f'data/{dirname}/'
 
     dataset = pd.read_csv(f"{path}/{inputfile}", index_col=0)
     V = dataset.values
-
+ 
     for row in V:
-        n = int(row[2])
-        m = int(row[1])
+        n = int(row[2]) # number of raters
+        m = int(row[1]) # number of raters giving positive label
         pos_num = m
         for i in range(m):
+            # for each positive label, "noise" probability of turning it to negative 
             if noise.draw():
                 pos_num -= 1
         for i in range(n-m):
+            # for each negative label, "noise" probability of turning it to positive
             if noise.draw():
                 pos_num += 1
+        # save the new counts in "row"
         row[0]= pos_num*1.0/n
         row[1]= pos_num
 
